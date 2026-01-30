668228684372930d66bb7379dc8f478c4b32287c
CCat <61354181+CCathlete@users.noreply.github.com>
2026-01-30 22:45:51 +0000

    Skeleton created.

---
+++ analytics_assistant/src/main/java/com/catgineer/analytics_assistant/AnalyticsAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.catgineer.analytics_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsAssistantApplication.class, args);
+	}
+
+}
