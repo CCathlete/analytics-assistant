d126c9db7fd7a40f7a4ab9381aba8799e5f8ad63
ccathlete <ccathlete01@gmail.com>
2026-02-02 18:32:18 +0100

    Added a CORS configurerr class.

---
+++ analytics_assistant/src/main/java/com/catgineer/analytics_assistant/control/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.catgineer.analytics_assistant.control.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
