abf5bffb1434cf933d4f775530976e2035ae5378
ccathlete <ccathlete01@gmail.com>
2026-02-02 14:54:27 +0100

    Moved the main entry point to project root.

--- analytics_assistant/src/main/java/com/catgineer/analytics_assistant/AnalyticsAssistantApplication.java
+++ analytics_assistant/src/main/java/com/catgineer/analytics_assistant/AnalyticsAssistantApplication.java
@@ -1,4 +1,4 @@
-package com.catgineer.analytics_assistant.control;
+package com.catgineer.analytics_assistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
