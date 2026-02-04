79f8223fcad0889b2f46b437c46f92a66e0564fe
ccathlete <ccathlete01@gmail.com>
2026-02-04 01:21:51 +0100

    Improved react components #3.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxying API requests to the backend
+      '/api': {
+        target: 'http://localhost:8383',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
